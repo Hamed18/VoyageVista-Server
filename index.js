b2884ac2b3c3887ab8fae5d96afe930b0c05b450
hamed18 <hamedcnec@gmail.com>
2024-08-25 14:45:16 +0600

    Get data from mongo database. before that, insert json data in mongogdb from atlas

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//console.log(process.env.DB_USER)
-//console.log(process.env.DB_PASS)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3nkfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-//console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,6 +23,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+	// READ data stored in database
+	const SpotsCollection = client.db("VoyageVista").collection("TouristSpots");
+    app.get('/TouristSpots', async(req,res) => {
+		const cursor = SpotsCollection.find();
+		const result = await cursor.toArray();
+		res.send(result);
+	})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -38,9 +44,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req,res) => {
-	res.send('coffee making server is running')
+	res.send('tourism server is running')
 })
 
 app.listen(port, () => {
-	console.log(`coffee server is running on port ${port}`)
+	console.log(`tourism is running on port ${port}`)
 })
