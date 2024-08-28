d4cf1e625b4f3cc6963fc86b6e3ee02bdadd0516
hamed18 <hamedcnec@gmail.com>
2024-08-29 01:37:21 +0600

    DELETE: delete api

--- index.js
+++ index.js
@@ -66,6 +66,15 @@ async function run() {
 		res.send(result);
 	})
 
+	// DELETE: delete api to delete a data from My List
+	app.delete('/AllSpots/:id', async (req, res) => {
+		const id = req.params.id;
+		const query = { _id: new ObjectId(id) }
+		const result = await AllUsersAddedCollection.deleteOne(query);
+		res.send(result);
+	})
+	
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
